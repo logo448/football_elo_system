4640b628c0259bb4053ccf23c9267ae2f691efaf
logo448 <stenzellogan@gmail.com>
2015-06-08 14:34:32 -0500

    Added a function that calculates the probability of a team winning.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 # Logan Stenzel
 # NFL Elo rating system main file
 # the a and b variables represent team a and team b in the code
+
+
+def win_probability(a, b):
+    return 1.0/(10**(-(a-b)/400.0)+1)
