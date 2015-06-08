befacf7512ae78e0b10703b777057d4e7921fa37
logo448 <stenzellogan@gmail.com>
2015-06-08 16:48:04 -0500

    Added a function that calculates the new Elo ratings.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # the a and b variables represent team a and team b in the code
 import math
 
+_k = 20
+
 
 def win_probability(a, b):
     return 1.0/(10**(-(a-b)/400.0)+1)
@@ -10,3 +12,16 @@ def win_probability(a, b):
 
 def margin_victory_multiplier(win, lose, pd):
     return (math.log(abs(pd)+1)) * (2.2/((win-lose)*.001+2.2))
+
+
+def new_elo(a, b, a_win_bool, pd):
+    if a_win_bool == 1:
+        win = a
+        lose = b
+        change = _k*margin_victory_multiplier(win, lose, pd)
+        return a + change, b + (1-change)
+    else:
+        win = b
+        lose = a
+        change = _k*margin_victory_multiplier(win, lose, pd)
+        return a + (1-change), b + change
